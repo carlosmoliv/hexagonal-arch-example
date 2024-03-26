22ec6e42b598c97b8b04e0d7845eba264a9b9dbe
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-03-26 17:23:10 -0300

    feat: add AlarmFactory

---
+++ src/alarm/domain/factories/alarm.factory.ts
@@ -0,0 +1,13 @@
+import { randomUUID } from 'crypto';
+import { AlarmSeverity } from '../value-objects/alarm-severity';
+import { Alarm } from '../alarm';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AlarmFactory {
+  create(name: string, severity: string) {
+    const alarmId = randomUUID();
+    const alarmSeverity = new AlarmSeverity(severity as AlarmSeverity['value']);
+    return new Alarm(alarmId, name, alarmSeverity);
+  }
+}
