cf41921880c4de47fe58da4a2a6a5959cca80a0b
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-03-27 10:14:07 -0300

    feat: add AlarmRepository port

--- src/alarm/application/ports/alarm.repository.ts
+++ src/alarm/application/ports/alarm.repository.ts
@@ -2,5 +2,5 @@ import { Alarm } from '../../domain/alarm';
 
 export abstract class AlarmRepository {
   abstract findAll(): Promise<Alarm[]>;
-  abstract save(): Promise<Alarm>;
+  abstract save(alarm: Alarm): Promise<Alarm>;
 }
