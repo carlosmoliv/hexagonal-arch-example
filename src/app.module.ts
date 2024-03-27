a0ac71c250f78681919dd7952bd8bdad9db01947
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-03-27 14:44:15 -0300

    feat: enhance AppModule registration with dynamic configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AlarmModule } from './alarm/application/alarm.module';
+import { AlarmModule } from './alarm/alarm.module';
+import { CoreModule } from './core/core.module';
+import { ApplicationBootstrapOptions } from './common/interfaces/application-bootstrap-options';
+import { AlarmInfrastructureModule } from './alarm/infrastructure/alarm-infrastructure.module';
 
 @Module({
-  imports: [AlarmModule],
+  imports: [CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  static register(options: ApplicationBootstrapOptions) {
+    return {
+      module: AppModule,
+      imports: [
+        CoreModule.forRoot(options),
+        AlarmModule.withInfrastructure(
+          AlarmInfrastructureModule.use(options.driver),
+        ),
+      ],
+    };
+  }
+}
