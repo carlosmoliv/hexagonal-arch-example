75964ec757b7e4fb7c37ff16501cc3e0bad14d07
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-03-28 14:24:40 -0300

    feat: Implement CreateAlarmCommandHandler in AlarmService following CQRS pattern

--- src/alarm/alarm.module.ts
+++ src/alarm/alarm.module.ts
@@ -2,10 +2,11 @@ import { DynamicModule, Module, Type } from '@nestjs/common';
 import { AlarmController } from './presenters/http/alarm.controller';
 import { AlarmService } from './application/alarm.service';
 import { AlarmFactory } from './domain/factories/alarm.factory';
+import { CreateAlarmCommandHandler } from './application/commands/create-alarm-command-handler';
 
 @Module({
   controllers: [AlarmController],
-  providers: [AlarmService, AlarmFactory],
+  providers: [AlarmService, AlarmFactory, CreateAlarmCommandHandler],
 })
 export class AlarmModule {
   static withInfrastructure(infrastructureModule: Type | DynamicModule) {
