0fa2590f68cf56961a715d2d28f56f602ca2ce13
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-03-30 11:20:23 -0300

    refactor: replace AlarmRepository with CreateAlarmRepository in CreateAlarmCommandHandler

--- src/alarm/application/commands/create-alarm-command-handler.ts
+++ src/alarm/application/commands/create-alarm-command-handler.ts
@@ -1,9 +1,9 @@
 import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
 import { Logger } from '@nestjs/common';
 import { CreateAlarmCommand } from './create-alarm.command';
-import { AlarmRepository } from '../ports/alarm.repository';
 import { AlarmFactory } from '../../domain/factories/alarm.factory';
 import { AlarmCreatedEvent } from '../../domain/events/alarm-created.event';
+import { CreateAlarmRepository } from '../ports/create-alarm.repository';
 
 @CommandHandler(CreateAlarmCommand)
 export class CreateAlarmCommandHandler
@@ -12,7 +12,7 @@ export class CreateAlarmCommandHandler
   private readonly logger = new Logger(CreateAlarmCommandHandler.name);
 
   constructor(
-    private readonly alarmRepository: AlarmRepository,
+    private readonly alarmRepository: CreateAlarmRepository,
     private readonly alarmFactory: AlarmFactory,
     private readonly eventBus: EventBus,
   ) {}
