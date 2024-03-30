aabf8bac4c6accfaf24de25420d9cd39ed1b55f3
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-03-30 11:10:15 -0300

    feat: update AlarmCreatedEventHandler to insert the denormalized alarm document
    This event handler will insert the denormalized alarm document into the materialized view collection.

--- src/alarm/application/event-handlers/alarm-created.event-handler.ts
+++ src/alarm/application/event-handlers/alarm-created.event-handler.ts
@@ -1,13 +1,27 @@
-import { IEventHandler } from '@nestjs/cqrs';
+import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { AlarmCreatedEvent } from 'src/alarm/domain/events/alarm-created.event';
 import { Logger } from '@nestjs/common';
+import { UpsertMaterializedAlarmRepository } from '../ports/upsert-materialized-alarm.repository';
 
+@EventsHandler(AlarmCreatedEvent)
 export class AlarmCreatedEventHandler
   implements IEventHandler<AlarmCreatedEvent>
 {
   private readonly logger = new Logger(AlarmCreatedEventHandler.name);
 
-  handle(event: AlarmCreatedEvent) {
+  constructor(
+    private readonly upsertMaterializedRepository: UpsertMaterializedAlarmRepository,
+  ) {}
+
+  async handle(event: AlarmCreatedEvent) {
     this.logger.log(`Alarm created event: ${JSON.stringify(event)}`);
+    await this.upsertMaterializedRepository.upsert({
+      id: event.alarm.id,
+      name: event.alarm.name,
+      severity: event.alarm.severity.value,
+      triggeredAt: event.alarm.triggeredAt,
+      isAcknowledge: event.alarm.isAcknowledge,
+      items: event.alarm.items,
+    });
   }
 }
