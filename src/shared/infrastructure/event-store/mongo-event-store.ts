6583f242282d4cd4d3b10e944b237dd5da436469
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-03-31 14:37:22 -0300

    feat: implement MongoEventStore service
    Implement the MongoEventStore service, responsible for persisting serialized events to a MongoDB database. This service integrates with the MongoDB database using Mongoose, providing functionalities to handle transactions and error handling during event persistence.

---
+++ src/shared/infrastructure/event-store/mongo-event-store.ts
@@ -0,0 +1,42 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Event } from './schemas/event.schema';
+import { EVENT_STORE_CONNECTION } from '../../../core/core.constants';
+import { ISerializableEvent } from '../../domain/interfaces/serializable-event';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class MongoEventStore {
+  private readonly logger = new Logger(MongoEventStore.name);
+
+  constructor(
+    @InjectModel(Event.name, EVENT_STORE_CONNECTION)
+    private readonly eventStore: Model<Event>,
+  ) {}
+
+  async persist(
+    eventOrEvents: ISerializableEvent | ISerializableEvent[],
+  ): Promise<void> {
+    const events = Array.isArray(eventOrEvents)
+      ? eventOrEvents
+      : [eventOrEvents];
+    const session = await this.eventStore.startSession();
+    try {
+      session.startTransaction();
+      await this.eventStore.insertMany(events, { session, ordered: true });
+      await session.commitTransaction();
+      this.logger.debug(`Events inserted successfully to the event store`);
+    } catch (error) {
+      await session.abortTransaction();
+      const UNIQUE_CONSTRAINT_ERROR_CODE = 11000;
+      if (error?.code === UNIQUE_CONSTRAINT_ERROR_CODE) {
+        this.logger.error(`Events could not be persisted. Aggregate is stale.`);
+        console.error(error.writeErrors?.[0]?.err?.errmsg);
+      } else {
+        throw error;
+      }
+    } finally {
+      await session.endSession();
+    }
+  }
+}
