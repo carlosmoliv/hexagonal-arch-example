e60d17e12f33c490514851bd38ff1eb61120abb2
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-04-10 12:02:33 -0300

    refactor(alarm-created-event-handler): update Event Handling for Serialized Events

--- src/alarm/application/event-handlers/alarm-created.event-handler.ts
+++ src/alarm/application/event-handlers/alarm-created.event-handler.ts
@@ -2,10 +2,11 @@ import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { AlarmCreatedEvent } from 'src/alarm/domain/events/alarm-created.event';
 import { Logger } from '@nestjs/common';
 import { UpsertMaterializedAlarmRepository } from '../ports/upsert-materialized-alarm.repository';
+import { SerializedEventPayload } from '../../../shared/domain/interfaces/serializable-event';
 
 @EventsHandler(AlarmCreatedEvent)
 export class AlarmCreatedEventHandler
-  implements IEventHandler<AlarmCreatedEvent>
+  implements IEventHandler<SerializedEventPayload<AlarmCreatedEvent>>
 {
   private readonly logger = new Logger(AlarmCreatedEventHandler.name);
 
@@ -13,7 +14,7 @@ export class AlarmCreatedEventHandler
     private readonly upsertMaterializedRepository: UpsertMaterializedAlarmRepository,
   ) {}
 
-  async handle(event: AlarmCreatedEvent) {
+  async handle(event: SerializedEventPayload<AlarmCreatedEvent>) {
     this.logger.log(`Alarm created event: ${JSON.stringify(event)}`);
 
     // TODO: Implement a message broker to avoid inconsistency between the AlarmReadModel and the creation of an Alarm
@@ -22,7 +23,7 @@ export class AlarmCreatedEventHandler
       id: event.alarm.id,
       name: event.alarm.name,
       severity: event.alarm.severity.value,
-      triggeredAt: event.alarm.triggeredAt,
+      triggeredAt: new Date(event.alarm.triggeredAt),
       isAcknowledge: event.alarm.isAcknowledge,
       items: event.alarm.items,
     });
